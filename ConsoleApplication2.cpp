e47908d5d20dc6e978d41ce7edcd43c0d5dd3e9f
Nikita Ivashin <n.ivashin@yandex.ru>
2020-06-30 02:24:06 +0700

    factorial

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a;
+    unsigned long long result = 1; //не уверен, что это си
+    printf("%d", result);
+
+    printf("Write two numbers:\n");
+    scanf("%d", &a);  //он говорит, что не учитывается возвращаемое значение. Я раньше так делал
+    for (int i = 1; i <= a; i++) {
+        result *= i;
+    }
+    printf("%d", result);
+
+
+}
