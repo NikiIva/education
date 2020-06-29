ada4a3bb30a55a9ec2ee3cbfe39d2c6319c34ca8
Nikita <n.ivashin@yandex.ru>
2020-06-30 01:36:03 +0700

    euclidean algorithm

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+
+int main()
+{
+    int a = 30;
+    int b = 18;
+    printf("%d",gcd(a,b));
+}
