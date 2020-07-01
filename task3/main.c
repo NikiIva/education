73d4578ff1fde9be6f306a8d8e66162bb9bf1415
Nikita <n.ivashin@yandex.ru>
2020-07-01 22:48:10 +0700

    update

--- task3/main.c
+++ task3/main.c
@@ -1,42 +1,15 @@
 #include "task.h"
 #include <stdio.h>
 
-void converter(int size,char * chars) {
-    bool canBeConversed = true;
-    int result = 0;
-
-    for (int i = 0; i < size; i++) {
-
-        if (chars[i] >= '0' && chars[i] <= '9') {
-            result = result * 10 + (chars[i] - '0');
-        }
-        else if (chars[i] == 0) {
-            break;
-        }
-        else {
-            canBeConversed = false;
-            printf("This string can`t be conversed.");
-            break;
-        }
-    }
-
-    if (canBeConversed)
-        printf("\nThe number is %d", result);
-}
-
-
 int main(){
     int size;
     printf("Write size of string_to_int:\n");
     scanf("%d", &size);
     size++;
-
     char* chars = (char*)malloc((size) * sizeof(char));
     printf("Write string_to_int:\n");
     scanf("%s", chars);
- 
-    converter(size, chars);
-   
-
+    int result = converter(size, chars);
+    printf("%d", result);
     free(chars);
 }
--- task3/task.c
+++ task3/task.c
@@ -1,9 +1,21 @@
 #include "task.h"
 
-unsigned long long factorial(int a) {
-    unsigned long long result = 1;
-    for (int i = 1; i <= a; i++) {
-        result *= i;
+int converter(int size, char* chars) {    
+    int result = 0;
+
+    for (int i = 0; i < size; i++) {
+
+        if (chars[i] >= '0' && chars[i] <= '9') {
+            result = result * 10 + (chars[i] - '0');
+        }
+        else if (chars[i] == 0) {
+            break;
+        }
+        else {           
+            printf("This string can`t be convertated.");
+            return;
+        }
     }
+
     return result;
 }
