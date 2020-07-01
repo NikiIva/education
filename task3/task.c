7e67b513d73006bdea4076fd5124cad292532e82
Nikita <n.ivashin@yandex.ru>
2020-07-01 22:39:54 +0700

    update

---
+++ task3/task.c
@@ -0,0 +1,9 @@
+#include "task.h"
+
+unsigned long long factorial(int a) {
+    unsigned long long result = 1;
+    for (int i = 1; i <= a; i++) {
+        result *= i;
+    }
+    return result;
+}
---
+++ task3/task.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define true 1
+#define false 0
+
+typedef unsigned short bool;
+
+unsigned long long factorial(int a);
